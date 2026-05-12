ff6514f54551dc7e444758928b440516d4b76d4b
thhuang3 <thhuang3@iflytek.com>
2026-05-12 10:42:01 +0800

    chore: bootstrap spring boot skeleton

---
+++ src/main/java/com/webox/webox/WeboxApplication.java
@@ -0,0 +1,11 @@
+package com.webox.webox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeboxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeboxApplication.class, args);
+    }
+}
---
+++ src/test/java/com/webox/webox/WeboxApplicationTests.java
@@ -0,0 +1,10 @@
+package com.webox.webox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeboxApplicationTests {
+    @Test
+    void contextLoads() {}
+}
